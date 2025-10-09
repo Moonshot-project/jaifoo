3f1346d82fa266a86f7a3b1609f672340d2e2a84
Sirakis Ngamwongwan <chojaokun@gmail.com>
2025-10-10 00:18:46 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+    output: "standalone",
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        unoptimized: true,
+    },
+};
 
-export default nextConfig
+console.log("=== SERVER STARTUP ===");
+console.log("BACKEND_API_SERVER:", process.env.BACKEND_API_SERVER);
+console.log("======================");
+
+export default nextConfig;
